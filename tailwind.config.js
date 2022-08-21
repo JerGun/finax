f33d9c543a5a2c2c4bfa725cf69e0b7b56031ac9
pawaret <62021393@up.ac.th>
2022-08-21 22:41:15 +0700

    Add colors

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...defaultTheme.colors,
+        ...{
+          background: "#0f0b15",
+          subBackground: "#402e4f",
+          primary: "#dd4831",
+          input: "#453b55",
+          placeholder: "#7F788D",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
